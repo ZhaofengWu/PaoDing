bec042b91870bd2fd7dd6d308b4103622bdce717
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2021-08-11 01:01:06 -0700

    Move requirements in setup.py as MANIFEST.in seems complicated

--- setup.py
+++ setup.py
@@ -3,20 +3,6 @@ from setuptools import setup
 with open("README.md") as f:
     long_description = f.read()
 
-# Reference: https://github.com/PyTorchLightning/pytorch-lightning/blob/1.4.1/pytorch_lightning/setup_tools.py#L22
-with open("requirements.txt") as file:
-    lines = [line.strip() for line in file.readlines()]
-reqs = []
-for line in lines:
-    # filer all comments
-    if "#" in line:
-        line = line[: line.index("#")].strip()
-    # skip directly installed dependencies
-    if line.startswith("http") or "@http" in line:
-        continue
-    if line:  # if requirement is not empty
-        reqs.append(line)
-
 setup(
     name="PaoDing",
     version="0.0.1",
@@ -35,6 +21,6 @@ setup(
     author="Zhaofeng Wu",
     author_email="zfw7@cs.washington.edu",
     packages=["paoding"],
-    install_requires=reqs,
+    install_requires=["torch==1.9.0"],
     python_requires=">=3.9.0",
 )
