cd55d432fdbd688297ceb14195fcc97fdd755422
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2021-09-04 15:46:25 -0700

    Add pooler

---
+++ paoding/modules/pooler.py
@@ -0,0 +1,42 @@
+import logging
+
+import torch
+
+logger = logging.getLogger(__name__)
+
+POOLING_MODES = {"avg", "max"}
+
+
+class Pooler(torch.nn.Module):
+    def __init__(self, hparams):
+        assert hparams.pooling_mode in POOLING_MODES
+
+        super().__init__()
+        self.hparams = hparams
+
+    def forward(self, tensor: torch.Tensor):
+        """
+        Input:
+            tensor: (bsz, seq_len, emb_dim)
+
+        output:
+            pooled_tensor: (bsz, emb_dim)
+        """
+        if self.hparams.pooling_mode == "avg":
+            return tensor.mean(1)
+        elif self.hparams.pooling_mode == "max":
+            return tensor.max(1)
+        else:
+            raise KeyError("Unsupported pooling mode")
+
+    @staticmethod
+    def add_args(parser):
+        parser.add_argument(
+            "--pooling_mode",
+            default=None,
+            type=str,
+            required=True,
+            choices=POOLING_MODES,
+        )
+
+        return parser
