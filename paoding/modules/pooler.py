c7e7e064aa465e7fa81cf64ecd5b7688fda65928
Zhaofeng Wu <zhaofengw@allenai.org>
2021-09-26 22:18:36 -0700

    Allow the pooler to take a mask

--- paoding/modules/pooler.py
+++ paoding/modules/pooler.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 
+from allennlp.nn.util import masked_max, masked_mean
 import torch
 
 logger = logging.getLogger(__name__)
@@ -15,20 +16,31 @@ class Pooler(torch.nn.Module):
         super().__init__()
         self.hparams = hparams
 
-    def forward(self, tensor: torch.Tensor):
+    def forward(self, tensor: torch.Tensor, mask: torch.Tensor = None):
         """
         Input:
             tensor: (bsz, seq_len, emb_dim)
+            mask: (bsz, seq_len)
 
         output:
             pooled_tensor: (bsz, emb_dim)
         """
         if self.hparams.pooling_mode == "avg":
-            return tensor.mean(1)
+            if mask is None:
+                return tensor.mean(1)
+            else:
+                return masked_mean(tensor, mask.unsqueeze(-1), 1)
         elif self.hparams.pooling_mode == "max":
-            return tensor.max(1)
+            if mask is None:
+                return tensor.max(1)
+            else:
+                return masked_max(tensor, mask.unsqueeze(-1), 1)
         elif self.hparams.pooling_mode == "last":
-            return tensor[:, -1, :]
+            if mask is None:
+                return tensor[:, -1, :]
+            if mask is not None:
+                last_pos = mask.sum(-1) - 1  # (bsz,)
+                return tensor[torch.arange(tensor.shape[0]), last_pos]
         else:
             raise KeyError("Unsupported pooling mode")
 
