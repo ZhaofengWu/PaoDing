df255179eb4fd1a41fa0a7e7ab635290c356cc00
Zhaofeng Wu <wuzhaofeng1997@gmail.com>
2022-10-01 17:51:18 -0400

    doc

--- paoding/argument_parser.py
+++ paoding/argument_parser.py
@@ -8,11 +8,16 @@ class ArgumentParser(argparse.ArgumentParser):
         super().__init__(*args, **kwargs)
 
     def add_argument(self, *args, **kwargs) -> None:
-        # Add a conflict resolution mechanism.
-        # Note: this method in the superclass returns the created action, but I don't know how to
-        # easily do that in the case of a "cache hit", so I'm disabling return altogether. We don't
-        # typically need it anyway, and when we do I can figure something out.
+        """
+        Add a conflict resolution mechanism.
 
+        NOTE: Conflicts are only resolved when args/kwargs completely match. If the flag name
+        matches but not other attributes, the super class's add_argument will still raise an error.
+
+        NOTE: This method in the superclass returns the created action, but I don't know how to
+        easily do that in the case of a "cache hit", so I'm disabling return altogether. We don't
+        typically need it anyway, and when we do I can figure something out.
+        """
         # manually patch a few common non-hashable cases
         def make_hashable(e):
             if isinstance(e, (list, KeysView, set)):
