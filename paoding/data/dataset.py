more causal lm fixes/improvements

paoding/data/dataset.py
@@ -74,7 +74,6 @@ class Dataset:
                 logger.info(f"Saving dataset cache at {self.cache_path}")
                 self.dataset_dict.save_to_disk(self.cache_path)
 
-        # TODO: remove
         self.hparams.subsample_training_ratio = getattr(
             self.hparams, "subsample_training_ratio", None
         )
@@ -139,16 +138,12 @@ class Dataset:
 
     @property
     def label_key(self) -> str:
-        """
-        The key in the example dictionary for the label. Unlike for input fields, where we look for
-        the original field in the example dictionary under e.g. self.text_key and put it back under
-        e.g. "input_ids", labels typically do not need to be processed, so this same key is used
-        for both the label in the original example dictionary and the batch. For most tasks, we
-        default to looking for the "label" key (i.e., the singular, just like "text"). But for LM,
-        because of this dual purpose, we have to match what `transformers` looks for in the batch,
-        which is the plural "labels". Maybe there's a way to make this cleaner...
-        """
-        return "label" if self.task != "causal-lm" else "labels"
+        return "label"
+
+    @property
+    def label_mask_key(self) -> str:
+        """The key in the example dictionary for the label mask, for tasks such as LM."""
+        return "label_mask"
 
     @property
     def sort_key(self) -> str | tuple[str, str]:
@@ -225,8 +220,22 @@ class Dataset:
             dataset_dict["dev"] = dataset_dict["validation"]
             del dataset_dict["validation"]
 
-        if self.task == "causal-lm":
-            dataset_dict = DatasetDict({k: d.add_column(self.label_key, d["input_ids"]) for k, d in dataset_dict.items()})
+        if self.task == "causal_lm":
+            dataset_dict = DatasetDict(
+                {
+                    k: d.map(
+                        lambda examples: {
+                            "input_ids": examples["input_ids"][:-1],
+                            "attention_mask": examples["attention_mask"][:-1],
+                            self.label_key: examples["input_ids"][1:],
+                            self.label_mask_key: examples["attention_mask"][1:],
+                        },
+                        batched=False,
+                        num_proc=4,
+                    )
+                    for k, d in dataset_dict.items()
+                }
+            )
 
         return dataset_dict
 
@@ -293,8 +302,10 @@ class Dataset:
         # We could default to using 0 like above, but `torchmetrics` doesn't support masks yet,
         # which makes it dangerous.
         assert self.tokenizer.pad_token_id is not None
-        label_pad = self.tokenizer.pad_token_id if self.task == "causal-lm" else None
+        label_pad = self.tokenizer.pad_token_id if self.task == "causal_lm" else None
         batch_info.update({self.label_key: (label_dtype, label_pad)})
+        if self.task == "causal_lm":
+            batch_info.update({self.label_mask_key: (torch.bool, False)})
         return batch_info
 
     def before_collation(self, batch: list[dict[str, list]]) -> list[dict[str, list]]:

paoding/models/model.py
@@ -58,7 +58,7 @@ class Model(pl.LightningModule):
         kwargs = {}
         if self.dataset.task == "classification":
             kwargs["num_classes"] = self.dataset.num_labels
-        if self.dataset.task == "causal-lm":
+        if self.dataset.task == "causal_lm":
             # TODO: this won't work for gpt2 now. See https://github.com/Lightning-AI/metrics/issues/54
             assert self.tokenizer.pad_token_id is not None
             kwargs["ignore_index"] = self.tokenizer.pad_token_id
@@ -168,19 +168,17 @@ class Model(pl.LightningModule):
     ) -> torch.Tensor:
         match self.dataset.task:
             case "classification":
-                loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), labels.view(-1))
-            case "token_classification":
+                loss = F.cross_entropy(logits.reshape(-1, logits.shape[-1]), labels.reshape(-1))
+            case "token_classification" | "causal_lm":
                 assert mask.any(dim=-1).all()
                 loss = F.cross_entropy(
-                    logits.view(-1, logits.shape[-1]), labels.view(-1), reduction="none"
+                    logits.reshape(-1, logits.shape[-1]), labels.reshape(-1), reduction="none"
                 )
-                loss = loss.view_as(labels) * mask
+                loss = loss.reshape_as(labels) * mask
                 if reduce:
                     loss = loss.sum() / mask.sum()
             case "regression":
-                loss = F.mse_loss(logits.view(-1), labels.view(-1))
-            # For some tasks such as causal-lm, you can directly use HF's corresponding model and
-            # the returned loss
+                loss = F.mse_loss(logits.reshape(-1), labels.reshape(-1))
             case _:
                 raise KeyError(f"Output mode not supported: {self.dataset.task}")
         return loss
@@ -191,7 +189,7 @@ class Model(pl.LightningModule):
             loss = output["loss"]
         else:
             loss = self.compute_loss(
-                output["logits"], batch[self.dataset.label_key], batch.get("label_mask")
+                output["logits"], batch[self.dataset.label_key], batch.get(self.dataset.label_mask_key)
             )
         self.log("train_loss", loss, on_step=False, on_epoch=True)
         self.log("lr", self.trainer.lr_schedulers[0]["scheduler"].get_last_lr()[-1], prog_bar=True)
@@ -203,12 +201,12 @@ class Model(pl.LightningModule):
                 return logits.argmax(dim=-1)
             case "regression":
                 return logits.squeeze(dim=-1)
-            case "causal-lm":
+            case "causal_lm":
                 # Perplexity is a weird metric as it needs the raw distribution... So this is
                 # actually not a prediction, and we need to do something else for real decoding.
                 # But then, the problem there is more complicated anyway since there's beam search
                 # etc.
-                return logits[..., :-1, :]
+                return logits
             case _:
                 raise KeyError(f"Output mode not supported: {self.dataset.task}")
 
@@ -222,8 +220,6 @@ class Model(pl.LightningModule):
         output = self(batch)
         preds = self.get_predictions(output["logits"], batch)
         labels = batch[self.dataset.label_key]
-        if self.dataset.task == "causal-lm":
-            labels = labels[..., 1:]
 
         for s in (split, "aggregate"):
             for metric in self.metrics[s].values():
@@ -238,7 +234,7 @@ class Model(pl.LightningModule):
             loss = (
                 output["loss"]
                 if "loss" in output
-                else self.compute_loss(output["logits"], labels, batch.get("label_mask"))
+                else self.compute_loss(output["logits"], labels, batch.get(self.dataset.label_mask_key))
             )
             return_dict["loss"] = loss.detach().cpu()
         return return_dict
@@ -262,6 +258,11 @@ class Model(pl.LightningModule):
 
         for split, split_outputs in zip(splits, outputs, strict=True):
             metrics = self.get_metrics(split, reset=True)
+
+            if "loss" in split_outputs[0]:
+                loss = sum(o["loss"] for o in split_outputs) / len(split_outputs)
+                metrics |= {"loss": loss}
+
             for k, v in metrics.items():
                 if num_splits > 1:
                     assert f"{k}_{split}" not in metrics