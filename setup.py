dfe6fd35876af3c39d0e85ee53a21a5955d7377e
Zhaofeng Wu <zhaofengw@allenai.org>
2022-12-02 11:58:41 -0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="PaoDing",
-    version="0.1.0",
+    version="0.1.1",
     description="An NLP-oriented PyTorch wrapper that makes your life easier.",
     license="Apache 2.0",
     long_description=long_description,
