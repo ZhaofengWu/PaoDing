6fdff3672d3c28e5e131cf0355ed524b624db478
Zhaofeng Wu <zfw@meta.com>
2024-06-28 19:56:27 +0000

    Support more customizable length sorting

--- paoding/data/dataset.py
+++ paoding/data/dataset.py
@@ -402,6 +402,11 @@ class Dataset:
             return cpu_count
         return 2
 
+    def get_length_for_sorting(self, value: Any) -> int:
+        """"Sometimes when value is a nested Tensor, we may want the length at different dims. Give
+        the subclass a chance to overwrite this."""
+        return len(value)
+
     def dataloader(self, split: str, batch_size: int, shuffle=False) -> DataLoader:
         dataset_split = self.dataset_dict[split]
         sampler = None
@@ -412,7 +417,7 @@ class Dataset:
             for k in self.sort_key if not isinstance(self.sort_key, str) else [self.sort_key]:
                 for i, v in enumerate(dataset_split[k]):  # TODO: this is a bit slow
                     # LengthGroupedSampler sorts from longest to shortest; we want the reverse
-                    lens[i] -= len(v)
+                    lens[i] -= self.get_length_for_sorting(v)
             if self.hparams.gpus <= 1:
                 sampler = LengthGroupedSampler(batch_size, lengths=lens)
             else:
